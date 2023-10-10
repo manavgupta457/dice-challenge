ac737a8b4f35833406e78e18cc9489528c120e8f
Manav Gupta <67089954+manavgupta457@users.noreply.github.com>
2023-10-10 18:42:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+var tempL=Math.random()*6;
+tempL=Math.floor(tempL)+1;
+var tempR=Math.random()*6;
+tempR=Math.floor(tempR)+1;
+// while(randomNumber1==randomNumber2){
+//     tempR=Math.random()*6;
+//     tempR=Math.floor(tempR)+1;
+// }
+var strL="./images/dice"+tempL+".png";
+var strR="./images/dice"+tempR+".png";
+document.querySelector("body > div > div:nth-child(2) > img").setAttribute("src",strL);
+document.querySelector("body > div > div:nth-child(3) > img").setAttribute("src",strR);
+
+if(tempL>tempR){
+    document.querySelector("body > div > h1").innerHTML="Player 1 wins 🚩";
+}
+else if(tempR>tempL){
+    document.querySelector("body > div > h1").innerHTML="Player 2 wins 🚩";
+}
+else{
+    document.querySelector("body > div > h1").innerHTML="Draw!";
+}
